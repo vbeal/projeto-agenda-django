477bb3c47ff0db6f545fd6accf33309f70bd9fb0
VBeal <victorbeal@gmail.com>
2024-12-10 08:14:43 -0300

    Inserido ação do acesso aos forms

--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -1,1 +1,9 @@
 from .contact_views import *
+from .contact_forms import *
+'''
+Atenção como estamos importando tudo dos arquivos contact_views e contact_forms
+ele vai pegar tudo que tem dentro desses arquivos
+assim eles não podem ter o mesmo nome de view. 
+Ex: def index(request) no contact_views e def index(request) no contact_forms
+Isso vai gerar um erro, pois eles tem o mesmo nome
+'''
