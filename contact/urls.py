9405a8d30ff7d72320d0c1166c0f8635f3f997b3
VBeal <victorbeal@gmail.com>
2024-12-09 22:55:44 -0300

    Update urls.py
    Criando o padrão para CRUD nas páginas para facilitar no futuro além de novas urls

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,12 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
+    path('search/', views.search, name='search'), 
     path('', views.index, name='index'),
+
+    #Padrão CRUD
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/update/', views.contact, name='contact'), 
+    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'), 
+    # path('contact/create/', views.contact, name='contact'), 
 ]
